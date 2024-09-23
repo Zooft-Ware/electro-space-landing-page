bce6b87d81b57544f2a3be0bed450299759986d8
Ilan Arellano <72232853+IlanArellano@users.noreply.github.com>
2024-09-23 12:54:10 -0600

    Change Image Route

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zooft-ware.github.io',
-  base: 'electro-space-landing-page',
-  integrations: [tailwind(), react()]
+  site: "https://zooft-ware.github.io",
+  base: "electro-space-landing-page",
+  integrations: [tailwind(), react()],
+  image: {
+    service: passthroughImageService(),
+  },
 });
